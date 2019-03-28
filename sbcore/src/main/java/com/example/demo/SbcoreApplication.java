54302c95422094b04f39246ee3a21ebf1a3a5acd
siddarth <siddarthakamble427@gmail.com>
2019-03-28 19:25:24 +0530

    sbcore

--- sbcore/src/main/java/com/example/demo/SbcoreApplication.java
+++ sbcore/src/main/java/com/example/demo/SbcoreApplication.java
@@ -1,5 +1,13 @@
 package com.example.demo;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,11 +22,30 @@ import com.nt.beans.Motor;
 @ComponentScan(basePackages= {"com.nt.bean,com.nt.beans"})
 public class SbcoreApplication {
 	
-
+public static Logger loger=Logger.getLogger(SbcoreApplication.class);
+	@PostConstruct
+	public void init() {
+		loger.info("before initialized...");
+		
+	}
+	@PreDestroy
+	public Set<String> destroyt() {
+		loger.info("after distroying...");
+		Set<String> names= new HashSet<>();
+		names.add(new String("siddu"));
+		names.add("kamble");
+		names.add("hyd");
+		names.add("somajiguda");
+		return names;
+		
+	}
 	public static void main(String[] args) {
 		Motor motor=null;
 	ApplicationContext context=	SpringApplication.run(SbcoreApplication.class, args);
 	motor = context.getBean("motor",Motor.class);
+	SbcoreApplication run= new SbcoreApplication();
+	Set<String> name=run.destroyt();
+	System.out.println(name);
 	System.out.println(motor);
 	motor.run();
 	
